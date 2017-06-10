df8124c49aa635b7aba17da4424b5c99c651b167
Vish <svaradhan92@gmail.com>
2017-06-10 16:06:17 +1000

    * Added support for network listener * Added support for loading data from paginated API * Average cubic weight calculation

---
+++ CWCalculator/app/src/main/java/com/kogan/cwcalculator/AirConditioner.java
@@ -0,0 +1,44 @@
+package com.kogan.cwcalculator;
+
+/**
+ * Created by Vish on 10/06/2017.
+ */
+public class AirConditioner {
+
+    private double length;
+    private double width;
+    private double height;
+    private  double cubicWeight;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getCubicWeight() {
+        return cubicWeight;
+    }
+
+    public void setCubicWeight(double cubicWeight) {
+        this.cubicWeight = cubicWeight;
+    }
+}
--- CWCalculator/app/src/main/java/com/kogan/cwcalculator/MainActivity.java
+++ CWCalculator/app/src/main/java/com/kogan/cwcalculator/MainActivity.java
@@ -2,12 +2,146 @@ package com.kogan.cwcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button triggerApi;
+    private TextView label;
+    private TextView resultView;
+
+    private String baseUrl;
+    private HashMap<Integer,AirConditioner> airConditioners;
+    private double conversionFactor;
+    private String productName;
+    private Random random;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeUi();
     }
+
+    private void initializeUi(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.action_menu_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        boolean networkStatus = NetworkStatusChecker.getNetworkConnectivity(this);
+
+        if(networkStatus){
+            triggerApi = (Button)findViewById(R.id.button);
+            label = (TextView)findViewById(R.id.tv_label);
+            resultView = (TextView)findViewById(R.id.tv_avgcw);
+
+            airConditioners = new HashMap<>();
+            random = new Random();
+            conversionFactor = 250;
+            productName = "Air Conditioners";
+            baseUrl = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com";
+            triggerApi.setOnClickListener(apiTriggerListener);
+        }
+        else
+            Toast.makeText(this, "Please connect to internet", Toast.LENGTH_SHORT).show();
+    }
+
+    private View.OnClickListener apiTriggerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String requestEndPoint = "/api/products/1";
+            triggerAPIRequest(requestEndPoint);
+        }
+    };
+
+    private void triggerAPIRequest(String endPoint){
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, baseUrl + endPoint, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                extractDataFromResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+    private void extractDataFromResponse(JSONObject response){
+        try {
+            if(response.has("objects")){
+                JSONArray objectsReceived = (JSONArray) response.get("objects");
+                for(int i =0; i < objectsReceived.length(); i++){
+                    JSONObject productJson  = (JSONObject) objectsReceived.get(i);
+                    Log.d("Category - "+i,productJson.getString("category"));
+                    if(productJson.getString("category").equals(productName)){
+                        AirConditioner airConditioner = new AirConditioner();
+                        JSONObject size = (JSONObject)productJson.get("size");
+                        airConditioner.setHeight(Double.parseDouble(size.getString("height")));
+                        airConditioner.setWidth(Double.parseDouble(size.getString("width")));
+                        airConditioner.setLength(Double.parseDouble(size.getString("length")));
+                        double cubicWeight = (airConditioner.getLength()*airConditioner.getHeight()*airConditioner.getWidth()*conversionFactor)/1000000;
+                        airConditioner.setCubicWeight(cubicWeight);
+                        int key = random.nextInt();
+                        if(!airConditioners.containsKey(key))
+                            airConditioners.put(key,airConditioner);
+                    }
+
+                }
+            }
+
+            if(!response.getString("next").equals("null")){
+                triggerAPIRequest(response.getString("next"));
+            }
+            else{
+                calculateAverageCubicWeight();
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void calculateAverageCubicWeight(){
+        double sum = 0;
+        for(Map.Entry<Integer,AirConditioner> entry : airConditioners.entrySet()){
+            AirConditioner tempAirConditioner = (AirConditioner)entry.getValue();
+            Log.d(entry.getKey()+"",tempAirConditioner.getCubicWeight()+"");
+            sum += tempAirConditioner.getCubicWeight();
+        }
+
+        resultView.setText(String.format("%.2f",sum/airConditioners.size())+"kg");
+        resultView.setVisibility(View.VISIBLE);
+        label.setVisibility(View.VISIBLE);
+        triggerApi.setVisibility(View.INVISIBLE);
+    }
+
 }
